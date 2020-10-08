b0209c9600ee58c8081ba0b3a1ceb9d8bc325795
Sonyei <esonyei@gmail.com>
2020-10-08 19:01:51 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Displays from './Displays'
+import Carousel from './Carousel'
+import styled from 'styled-components'
 import './App.css';
+
+const StyledHeader = styled.div`
+  color: ${pr => pr.theme.nasaTitle};
+  font-weight: ${pr => pr.bold};
+`
 
 function App() {
   const [image, setImage] = useState({})
 
+
 useEffect(() => {
 const fetchPicture = () => {
-  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=11MZcjXaDbde3HCr8PtHAUd95sM1ug2yTqFNVQeN')
     .then(res => {
     console.log(res.data)
     setImage(res.data)
@@ -21,13 +29,12 @@ const fetchPicture = () => {
   }, [])
 
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <StyledHeader className="App">
+      <h1>
+        NASA: Astronomy Picture of the Day
+      </h1>
     <Displays imgInfo={image.url} title={image.title} copyright={image.copyright} date={image.date} imageDesc={image.explanation}/>
-    </div>
+    </StyledHeader>
   );
 }
 
--- src/Displays.js
+++ src/Displays.js
@@ -1,21 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
 
 
-const displayHeader = props => {
+const StyledWrapper = styled.div`
+    display: ${pr => pr.theme.display.display};
+    background-color: ${pr => pr.theme.bgColor};
+    color: ${pr=> pr.theme.textColor};
+    margin: ${pr => pr.theme.margin.large};
+    border: 2px solid ${pr => pr.theme.borderColor};
+    border-radius: 20px;
+    `
 
-    const {imgInfo, title, copyright, date, imageDesc} = props;
+const InfoBlock = styled.div`
+    display: ${pr => pr.theme.display.display};
+    flex-direction: ${pr => pr.theme.display.flexdir};
+    margin: ${pr => pr.theme.margin.large};
 
-    return (
-        <div className="Header">
-            <img src = {imgInfo} alt='Space'></img>
-            <h1>{title}</h1>
-            <p className='date'>Date: {date}</p>
-            <p>{imageDesc}</p>
-            <p className='copyright'>Copyright: {copyright}</p>
-
-            <button>Close</button>
-        </div>
-    )
+h1 {
+    color: ${pr => pr.theme.titlesColor};
+    font-size: ${pr => pr.theme.h1.fontsize};
+    width: ${pr => pr.theme.h1.width};
 }
 
-export default displayHeader;
+p {
+    color: ${pr => pr.theme.textColor};
+
+}
+`
+
+const BoldedP = styled.p`
+    font-weight: ${pr => pr.bold ? 'initial' : 'bold'};
+`
+
+export default function displayHeader(props) {
+
+    const { imgInfo, title, copyright, date, imageDesc, bold } = props
+
+    return (
+        <StyledWrapper>
+            <img src={imgInfo} alt='Space'></img>
+            <InfoBlock>
+                <h1>{title}</h1>
+                <BoldedP bold={bold}>Date: {date}</BoldedP>
+                <p>{imageDesc}</p>
+                <BoldedP bold={bold}>Copyright: {copyright}</BoldedP>
+            </InfoBlock>
+        </StyledWrapper>
+    )
+}
